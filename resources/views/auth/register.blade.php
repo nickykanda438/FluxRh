a40a4835ff848195712afe68a8b07381f7b2643d
T-Mutshipay <heritiermutshipay@gmail.com>
2026-03-21 08:26:24 +0100

    refactor : nettoyage des vues de connexion et d'inscription pour plus de cohérence et de lisibilité.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,44 +1,43 @@
-{{-- nous allons maintenant hériter de notre layaout inscription --}}
+@extends('auth.auth-layout')
 
-@extends('auth.auth-layout') 
 
-{{--la définition de la section titre de la page--}}
-
-@section('title', "Page d'Inscription")
+@section('title', 'Register')
 
 @section('auth-form')
-							<h1 class="mb-3">S'inscrire</h1>
-							<form action="{{route('register')}}" method="POST">
-                                @csrf
-								<div class="form-group">
-									<input class="form-control" type="text" name="name" value="{{ old('name') }}" placeholder="Nom"> 
-								</div>
-                                    @error('name')
-                                         <p class="text-red-500 mt-2">{{$message}}</p>
-                                    @enderror
-								<div class="form-group">
-									<input class="form-control" type="email" name="email" value="{{ old('email') }}" placeholder="Email"> 
-								</div>
-                                    @error('email')
-                                         <p class="text-red-500 mt-2">{{$message}}</p>
-                                    @enderror
-								<div class="form-group">
-									<input class="form-control" type="password" name="password" value="{{ old('password') }}" placeholder="Mot de passe"> 
-                                </div>
-                                    @error('password')
-                                         <p class="text-red-500 mt-2">{{$message}}</p>
-                                    @enderror
-								<div class="form-group">
-									<input class="form-control" type="password" name="password_confirmation" value="{{ old('password_confirmation') }}" placeholder="Confirmer Mot de passe"> </div>
-                                    @error('password_confirmation')
-                                         <p class="text-red-500 mt-2">{{$message}}</p>
-                                    @enderror
-								<div class="form-group mb-0">
-									<button class="btn btn-primary btn-block" type="submit">S'inscrire</button>
-								</div>
-							</form>
-							
-							<div 
-                               class="text-center dont-have">Vous avez deja un compte? <a href="{{route('login')}}">Se connecter</a> 
-                            </div> 
+    <h1 class="mb-3">S'inscrire</h1>
+    <form action="{{ route('register') }}" method="POST">
+        @csrf
+        <div class="form-group">
+            <input class="form-control" type="text" name="name" value="{{ old('name') }}" placeholder="Nom">
+        </div>
+        @error('name')
+            <p class="text-red-500 mt-2">{{ $message }}</p>
+        @enderror
+        <div class="form-group">
+            <input class="form-control" type="email" name="email" value="{{ old('email') }}" placeholder="Email">
+        </div>
+        @error('email')
+            <p class="text-red-500 mt-2">{{ $message }}</p>
+        @enderror
+        <div class="form-group">
+            <input class="form-control" type="password" name="password" value="{{ old('password') }}"
+                placeholder="Mot de passe">
+        </div>
+        @error('password')
+            <p class="text-red-500 mt-2">{{ $message }}</p>
+        @enderror
+        <div class="form-group">
+            <input class="form-control" type="password" name="password_confirmation"
+                value="{{ old('password_confirmation') }}" placeholder="Confirmer Mot de passe">
+        </div>
+        @error('password_confirmation')
+            <p class="text-red-500 mt-2">{{ $message }}</p>
+        @enderror
+        <div class="form-group mb-0">
+            <button class="btn btn-primary btn-block" type="submit">S'inscrire</button>
+        </div>
+    </form>
+
+    <div class="text-center dont-have">Vous avez deja un compte? <a href="{{ route('login') }}">Se connecter</a>
+    </div>
 @endsection
