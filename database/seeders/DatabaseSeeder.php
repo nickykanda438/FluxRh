a4927c3336eddf94b77f0c10dff02a86c9fd5961
T-Mutshipay <heritiermutshipay@gmail.com>
2026-03-22 13:52:01 +0100

    feat(factory): ajout de la factory Agent avec des attributs générés aléatoirement

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Agent;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call([
             DivisionAndBureauSeeder::class,
+            Agent::factory(100)->create(),
         ]);
     }
 }
