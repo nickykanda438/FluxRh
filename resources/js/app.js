ebbd1d7ccc62073034411d945fd029947e9f0c81
T-Mutshipay <heritiermutshipay@gmail.com>
2026-03-22 07:46:08 +0100

    feat: Mise à jour de la mise en page et de la configuration de l'application pour un style et des fonctionnalités améliorés

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,6 @@ import 'flowbite';
 import { DataTable } from "simple-datatables";
 import Alpine from 'alpinejs';
 
-// En ajoutant cette ligne, le gris disparaîtra et DataTable sera accessible partout
 window.DataTable = DataTable; 
 
 window.Alpine = Alpine;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
+import flowbitePlugin from 'flowbite/plugin';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    // 1. Définition du mode sombre (indispensable pour Flowbite)
+    darkMode: 'class',
+
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
@@ -12,14 +16,20 @@ export default {
 
     theme: {
         extend: {
+            colors: {
+                primary: "#1a227f",
+                "background-light": "#f6f6f8",
+                "background-dark": "#121320",
+            },
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                display: ["Inter", "sans-serif"],
             },
         },
     },
 
     plugins: [
         forms,
-    require('flowbite/plugin',
-    )],
+        flowbitePlugin, 
+    ],
 };
